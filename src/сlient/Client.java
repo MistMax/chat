df16bcb9d6c456a660b8cafc6eee47919545bd14
Maxim <mistmax47@gmail.com>
2021-08-11 09:14:18 +0300

    чат группы 0988

---
+++ src/сlient/Client.java
@@ -0,0 +1,42 @@
+package сlient;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 8188);
+            System.out.println("Успешно подключен");
+            DataInputStream in = new DataInputStream(socket.getInputStream()); // Поток ввода
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // Поток вывода
+            String response = in.readUTF(); // Читаем ответ от сервера
+            System.out.println(response);
+            Scanner scanner = new Scanner(System.in);
+            out.writeUTF(scanner.nextLine()); // ожидаем ввод имени
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String response;
+                    while (true) { // Бесконечно ожидаем ответ от сервера
+                        try {
+                            response = in.readUTF(); // Читаем ответ от сервера
+                            System.out.println(response);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            while (true){
+                String consoleText = scanner.nextLine(); // Ждём сообщение от пользователя
+                out.writeUTF(consoleText); // Отправляем сообщение на сервер
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
